7dc24e3bb3718450e73d7250a2564d3cf8af12b8
Joseph Jeong <112719052+Josephjj224@users.noreply.github.com>
2024-10-15 03:42:36 -0500

    Title: 별 찍는 것을 5번 반복하기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241015/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.py
@@ -0,0 +1,14 @@
+def star(str):
+    for i in range(10):
+        print(str,  end="")
+
+
+star('*')
+print(end ="\n")
+star('*')
+print(end ="\n")
+star('*')
+print(end ="\n")
+star('*')
+print(end ="\n")
+star('*')
