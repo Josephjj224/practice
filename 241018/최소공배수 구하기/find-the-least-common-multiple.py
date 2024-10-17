1631162eb2acc7c31141d9ace0b1fa6e560ab225
Joseph Jeong <112719052+Josephjj224@users.noreply.github.com>
2024-10-17 16:50:40 -0500

    Title: 최소공배수 구하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/최소공배수 구하기/find-the-least-common-multiple.py
+++ 241018/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,12 +1,14 @@
 target =[]
 
 def lcm(a,b):
+    if a == b:
+        return min(a,b)
     for i in range(1, max(a,b) +1):
         if a * i in target:
             return a * i
             break
         target.append(i*b)
-    return 0
+    return 
 
 
 a, b = map(int, input().split())
